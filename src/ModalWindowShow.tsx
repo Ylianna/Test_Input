ce4c907b1aa12828b058211a00d26a8fb9129d9e
Ylianna <vyotskaya08@mail.ru>
2021-04-16 19:56:57 +0300

    Finish

--- src/ModalWindowShow.tsx
+++ src/ModalWindowShow.tsx
@@ -13,10 +13,12 @@ const ModalWindowShow: React.FC<ModalWindowShowPropsType> = ({isOpend, children,
     return (
         <div className={`${style.modal__wrapper} ${isOpend ? style.open : style.close}`}>
             <div className={style.modal__body}>
+                <div>
                 <div className={`${style.modal__close} ${style.close}`} onClick={onModalClose}>×</div>
                 <h2>{`Здравствуйте, ${title}`}</h2>
                 <hr/>
                 {children}
+                </div>
             </div>
         </div>
     )
